6f0a36edca125719ec0f7b92f7cb7442b5dd6871
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-11-04 16:04:41 -0600

    adds solution for ec1

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -8,20 +8,17 @@ function Toggle({children}) {
   const [on, setOn] = React.useState(false);
   const toggle = () => setOn(!on);
 
-  return React.Children.map(children, child =>
-    React.cloneElement(child, {on, toggle})
-  );
+  return React.Children.map(children, child => {
+    if (typeof child.type === 'string') return child;
+    const newChild = React.cloneElement(child, {on, toggle});
+    return newChild;
+  });
 }
 
-// 🐨 Flesh out each of these components
-
-// Accepts `on` and `children` props and returns `children` if `on` is true
 const ToggleOn = ({on, children}) => (on ? children : null);
 
-// Accepts `on` and `children` props and returns `children` if `on` is false
 const ToggleOff = ({on, children}) => (on ? null : children);
 
-// Accepts `on` and `toggle` props and returns the <Switch /> with those props.
 const ToggleButton = ({on, toggle}) => <Switch on={on} onClick={toggle} />;
 
 function App() {
@@ -30,6 +27,7 @@ function App() {
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <span>Hello</span>
         <ToggleButton />
       </Toggle>
     </div>
